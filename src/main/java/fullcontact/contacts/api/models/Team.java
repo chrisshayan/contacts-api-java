55b40fcb92b2a2e5fc0fe728a1e0364c47e0e10d
Matt Elliott <mattdelliott@me.com>
2017-12-08 14:48:47 -0700

    Updated teamMembers data type in model

--- src/main/java/fullcontact/contacts/api/models/Team.java
+++ src/main/java/fullcontact/contacts/api/models/Team.java
@@ -4,11 +4,12 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.Date;
+import java.util.List;
 
 public class Team {
     @Getter @Setter public String teamId;
     @Getter @Setter public TeamData teamData;
-    @Getter @Setter public String[] teamMembers;
+    @Getter @Setter public List<String> teamMembers;
     @Getter @Setter public Date created;
     @Getter @Setter public Date updated;
 }
