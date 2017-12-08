16dfb8a67083030174938d1aa081e441b6979e95
Matt Elliott <mattdelliott@me.com>
2017-12-08 14:44:22 -0700

    Updated data type for sharedBy in Contact

--- src/main/java/fullcontact/contacts/api/models/Contact.java
+++ src/main/java/fullcontact/contacts/api/models/Contact.java
@@ -3,11 +3,12 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.Date;
+import java.util.List;
 
 public class Contact {
     @Getter @Setter public String contactId;
     @Getter @Setter public String teamId;
-    @Getter @Setter public String[] sharedBy;
+    @Getter @Setter public List<String> sharedBy;
     @Getter @Setter public String etag;
     @Getter @Setter public Date created;
     @Getter @Setter public Date updated;
