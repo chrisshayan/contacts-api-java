e237064c4eae485204c4f7e34e3e80ba3d8d73a8
Matt Elliott <mattdelliott@me.com>
2017-12-08 14:50:21 -0700

    Updated triggers data type in model

--- src/main/java/fullcontact/contacts/api/models/Webhook.java
+++ src/main/java/fullcontact/contacts/api/models/Webhook.java
@@ -3,11 +3,12 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.Date;
+import java.util.List;
 
 public class Webhook {
     @Getter @Setter public String webhookId;
     @Getter @Setter public String url;
-    @Getter @Setter public String[] triggers;
+    @Getter @Setter public List<String> triggers;
     @Getter @Setter public Date created;
     @Getter @Setter public String accountId;
 }
